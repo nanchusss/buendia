6796c54dc851b5fd084cf4e6996eb11c8a78a1f6
nanchusss <nancyart83@gmail.com>
2024-12-15 22:39:04 +0100

    hashrouter

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,4 @@
-import {
-  BrowserRouter,
-  HashRouter,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog/Blog";
 import Header from "./components/Header/Header";
@@ -17,16 +11,6 @@ import ProtectedRoute from "./components/Protected-routes";
 import { useContext } from "react";
 import { MyContext } from "../src/Context";
 import Productes from "./components/Productes/productes";
-
-// Determina el tipo de Router según el entorno
-const RouterWrapper = ({ children }) => {
-  const isProduction = process.env.NODE_ENV === "production";
-  return isProduction ? (
-    <HashRouter>{children}</HashRouter>
-  ) : (
-    <BrowserRouter>{children}</BrowserRouter>
-  );
-};
 
 const Router = () => {
   const {
@@ -45,88 +29,13 @@ const Router = () => {
     setAlreadyLogged,
   } = useContext(MyContext);
 
-  if (login === true) {
-    return (
-      <>
-        <Login />
-        <Footer />
-      </>
-    );
-  }
-
-  if (showForm === true) {
-    return (
-      <>
-        <Header />
-        <ContactForm />
-      </>
-    );
-  }
-
-  if (state === true && alreadyLogged === false) {
-    return (
-      <>
-        <Login
-          login={login}
-          handleLogin={handleLogin}
-          handleSetAlreadyLogged={handleSetAlreadyLogged}
-        />
-        <Footer />
-      </>
-    );
-  }
-
-  if (alreadyLogged === true || state === true) {
-    return (
-      <>
-        <Header />
-        <InspirationPage />
-        <Footer />
-      </>
-    );
-  }
-
-  if (showBlog === true) {
-    return (
-      <>
-        <Header />
-        <Blog />
-        <Footer />
-      </>
-    );
-  }
-
-  if (showQuizz === true) {
-    return (
-      <>
-        <div style={{ position: "fixed", top: 0 }}>
-          <Header />
-        </div>
-        <Quiz />
-      </>
-    );
-  }
-
   return (
     <>
       <Header handleLogin={handleLogin} handleShowQuizz={handleShowQuizz} />
-
-      <RouterWrapper>
+      <HashRouter>
         <Routes>
           <Route
-            path="/login"
-            element={
-              <Login
-                alreadyLogged={alreadyLogged}
-                login={login}
-                handleLogin={handleLogin}
-                handleSetAlreadyLogged={handleSetAlreadyLogged}
-                setAlreadyLogged={setAlreadyLogged}
-              />
-            }
-          />
-          <Route
-            index
+            path="/"
             element={
               <Home
                 handleShowQuizz={handleShowQuizz}
@@ -139,13 +48,22 @@ const Router = () => {
               />
             }
           />
-          <Route path="/Blog/" element={<Blog />} />
-          <Route path="/Contacte/" element={<ContactForm />} />
-          <Route path="/Quizz/" element={<Quiz />} />
-          <Route path="/productes" element={<Productes />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contacte" element={<ContactForm />} />
+          <Route path="/Quizz" element={<Quiz />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Productes" element={<Productes />} />
+          <Route
+            path="/Inspiration"
+            element={
+              <ProtectedRoute alreadyLogged={alreadyLogged}>
+                <InspirationPage />
+              </ProtectedRoute>
+            }
+          />
           <Route path="*" element={<div>404 - Página no encontrada</div>} />
         </Routes>
-      </RouterWrapper>
+      </HashRouter>
       <Footer />
     </>
   );
