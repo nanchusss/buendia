b1989853c04bbe57dc9bbc4fbd3987b85660bbcd
nanchusss <nancyart83@gmail.com>
2024-12-17 22:55:22 +0100

    commi

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,43 @@
 import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
-import { Link } from "react-router-dom"; // Importa Link de react-router-dom
+import {
+  Navbar,
+  Nav,
+  Button,
+  Card,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
-import { NavbarContainer, Logo, NavLinks, styles } from "./Header-styles";
-import LogoMarca from "../../Images/Logo.jpeg";
+import {
+  NavbarContainer,
+  Logo,
+  NavLinks,
+  styles,
+  //ButtonsHeader,
+  //ButtonsHeader2,
+} from "./Header-styles";
+import LogoMarca from "..//.//../Images/Logo.jpeg";
 
-const Header = ({ handleShowQuizz, handleShowBlog, handleShowForm }) => {
+const Header = ({
+  handleLogin,
+  handleShowQuizz,
+  handleShowBlog,
+  handelShowForm,
+}) => {
+  //isLoggedIn posee la info de mail
+  const isLoggedIn = localStorage.getItem("email");
+
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    window.location.reload();
+    // Recarga la página para actualizar el estado de inicio de sesión, osea cierra sesión.
+  };
+
   return (
     <NavbarContainer bg="light" expand="lg" className="mb-5">
       <Navbar.Brand
+        href="#"
         style={{
           textAlign: "center",
           display: "flex",
@@ -19,7 +48,7 @@ const Header = ({ handleShowQuizz, handleShowBlog, handleShowForm }) => {
         <Logo
           src={LogoMarca}
           alt="Logo"
-          style={{ width: "80px", height: "80px", objectFit: "contain" }}
+          style={{ width: "80px", height: "80px", objectFit: "contain" }} // Ajusta el tamaño del logo
         />
         <span
           style={{
@@ -36,16 +65,17 @@ const Header = ({ handleShowQuizz, handleShowBlog, handleShowForm }) => {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <NavLinks className="mr-auto mt-3 ">
-          <Nav.Link as={Link} to="/" className="ml-2">
+          <Nav.Link style={styles.link} href="/" className="ml-2">
             Inici
           </Nav.Link>
-          <Nav.Link as={Link} to="/blog" onClick={handleShowBlog}>
+
+          <Nav.Link onClick={handleShowBlog} href="/blog">
             Blog
           </Nav.Link>
-          <Nav.Link as={Link} to="/contacte" onClick={handleShowForm}>
+          <Nav.Link onClick={handelShowForm} href="/contacte" className="ml-2">
             Contacte
           </Nav.Link>
-          <Nav.Link as={Link} to="/productes">
+          <Nav.Link style={styles.link} href="/productes" className="ml-2">
             Productes
           </Nav.Link>
         </NavLinks>
